a133d6aacdf12b200206543311db58e738cb2765
Abouamp <a.ampukajev@student.helmo.be>
2024-11-25 16:47:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
+import requests
 
+url = "https://official-joke-api.appspot.com/random_joke"
+reponse = requests.get(url)
+blague = reponse.json()
+
+print(f'{blague["setup"]}\n{blague["punchline"]}')
